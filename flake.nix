8fabcda17ac47747b9c7a5cfe50c47fe29a9d641
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-04-12 19:51:26 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# file: flake.nix
+{
+  description = "A GStreamer development flake";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+        gst_all_1.gstreamer
+        # Common plugins like "filesrc" to combine within e.g. gst-launch
+        gst_all_1.gst-plugins-base
+        # Specialized plugins separated by quality
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        # Plugins to reuse ffmpeg to play almost every video format
+        gst_all_1.gst-libav
+        # Support the Video Audio (Hardware) Acceleration API
+        gst_all_1.gst-vaapi
+        #...
+        xfce.xfburn
+      ];
+    };
+  };
+}
